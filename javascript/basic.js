1a6a3e9bcda5a2b9d0dd55fa1c5b3a36d5e27133
Turtu <razvan.turtu@yahoo.com>
2016-11-25 15:28:10 -0800

    basic functionaledasf added

--- javascript/basic.js
+++ javascript/basic.js
@@ -1,3 +1,15 @@
 var ct = document.getElementById( 'draw-canvas' );
 var ctx = ct.getContext( '2d' );
 ctx.fillRect( 0 , 0 , 1000 , 1000 );
+
+function ChangePageTitle( newName ){
+    document.title = newName;
+}
+
+function ChangeHeaderTitle( newName ){
+    document.getElementById( 'header-title' ).innerHTML = newName.toUpperCase();
+}
+
+function AddLogLine( newLine ){
+    document.getElementById( 'log-box' ).innerHTML = newLine + '<br>' + document.getElementById( 'log-box' ).innerHTML;
+}
