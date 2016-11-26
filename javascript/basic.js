2106aa0974ebd3c44ff6b62941e825e81ee477bd
Lolkekzor <chioreantudoroctavian@gmail.com>
2016-11-26 04:49:30 +0200

    no message

--- javascript/basic.js
+++ javascript/basic.js
@@ -13,3 +13,24 @@ function ChangeHeaderTitle( newName ){
 function AddLogLine( newLine ){
     document.getElementById( 'log-box' ).innerHTML = newLine + '<br>' + document.getElementById( 'log-box' ).innerHTML;
 }
+
+var menuToggle = false;
+var aux = document.getElementsByClassName( 'menu-item-div' );
+for( var i = 0 ; i < aux.length ; i++ ){
+    aux[ i ].style.display = 'none';
+}
+
+function ToggleMenu(){
+    var aux = document.getElementsByClassName( 'menu-item-div' );
+    if( !menuToggle ){
+        for( var i = 0 ; i < aux.length ; i++ ){
+            aux[ i ].style.display = 'block';
+        }
+    }
+    else{
+        for( var i = 0 ; i < aux.length ; i++ ){
+            aux[ i ].style.display = 'none';
+        }
+    }
+    menuToggle = !menuToggle;
+}
